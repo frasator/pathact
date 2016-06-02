1f8b0ba3017f266ecafacbf174f69cecd4863345
frasator <frasator@gmail.com>
2016-06-02 19:34:39 +0200

    Modified config.js

--- conf/config.js
+++ conf/config.js
@@ -7,6 +7,6 @@ STEVIA_SERVER_HOST = 'http://ws.babelomics.org/stv01';
 // STEVIA_SERVER_HOST = 'http://stvtest.babelomics.org';
 // STEVIA_SERVER_HOST = 'http://localhost:5555';
 
-STEVIA_SHOW_JOB_ERROR_FILES = true;
+// STEVIA_SHOW_JOB_ERROR_FILES = true;
 UPLOAD_FILES_WITH_ERRORS = true;
-STEVIA_SHOW_HIDDEN_FILES = false;
+// STEVIA_SHOW_HIDDEN_FILES = false;
